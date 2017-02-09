ab1829d509cb6e1079ad76117686beb07f1d2839
BerangereHetic <berangere.martin@hetic.net>
2017-02-09 21:34:46 +0100

    Modification of the wave

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var premier_bouton = document.querySelector('a.premierBouton');
 var deuxieme_bouton = document.querySelector('a.deuxiemeBouton');
+var troiseme_bouton = document.querySelector('a.troisiemeBouton');
 
 premier_bouton.addEventListener('click', function(event){
     event.preventDefault();
@@ -21,5 +22,10 @@ deuxieme_bouton.addEventListener('click', function(event){
 
 deuxieme_bouton.addEventListener('transitionend', function(){
     deuxieme_bouton.classList.remove('scale2');
+});
+
+troisieme_Bouton.addEventListener('click', function(event){
+    event.preventDefault();
+    troiseme_bouton.classList.add('scale');
 });
 
